84a905a6dcf9a2e3d48afa28733fc9ed896d8143
Darryl <darryl.dolan@hotmail.co.uk>
2018-03-05 17:35:21 +0100

    Manually added non gem JavaScript assets.

--- Gemfile
+++ Gemfile
@@ -30,9 +30,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'sprockets', '~> 3.7', '>= 3.7.1'
 gem 'sprockets-rails'
 gem 'popper_js'
 gem 'railties'
+gem "autoprefixer-rails"
 
 gem 'bootstrap', '~> 4.0'
 gem 'bootstrap-sass', '~> 3.3.6'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,26 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 // 
+//= require modernizr
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
+//= require jquery.appear
+//= require jquery.easing
+//= require jquery-cookie
 //= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require common
 //= require jquery.validate
+//= require jquery.stellar
 //= require jquery.easy-pie-chart
+//= require jquery.gmap
+//= require jquery.lazyload
+//= require jquery.isotope
 //= require owl.carousel
 //= require magnific-popup
+//= require vide
+//= require theme
+//= require theme.init
 //= require_tree .
