48e699d450768c969e74b6fa2d1a3627ba7dbca6
Darryl <darryl.dolan@hotmail.co.uk>
2018-07-04 11:40:37 +0200

    Removed unused test 'items' pages and assets.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,9 +9,10 @@ class PeopleController < ApplicationController
         @country = find_country_for_person(params[:id])
         render('show')
     end
-    # I'm sorry Daniel. Fix it please(It does work) :)
+    
     def find_country_for_person(person)
-        country = Country.find(Elite.find(PeopleElite.where("PersonId = #{person}").EliteId).CountryId)
+        # country = Country.find(Elite.find(PeopleElite.where("PersonId = #{person}").EliteId).CountryId)
+        Country.find(Elite.find(PeopleElite.first.EliteId).CountryId)
         
     end
 end
